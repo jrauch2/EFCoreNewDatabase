3261e6203e2dc954751b0284f64665e8ded3b45a
jgrissom <jgrissom91@gmail.com>
2019-01-24 21:34:30 -0600

    Create data repository
    1) Create the iBloggingRepository interface
    2) Implement the EFBloggingRepository class

---
+++ Models/EFBloggingRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace EFCoreNewDatabase.Models
+{
+    public class EFBloggingRepository : IBloggingRepository
+    {
+        // the repository class depends on the BloggingContext service
+        // which was registered at application startup
+        private BloggingContext context;
+        public EFBloggingRepository(BloggingContext ctx)
+        {
+            context = ctx;
+        }
+        // create IQueryable for Blogs & Posts
+        public IQueryable<Blog> Blogs => context.Blogs;
+        public IQueryable<Post> Posts => context.Posts;
+
+    }
+}
---
+++ Models/IBloggingRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace EFCoreNewDatabase.Models
+{
+    public interface IBloggingRepository
+    {
+        IQueryable<Blog> Blogs { get; }
+        IQueryable<Post> Posts { get; }
+
+        // TODO: AddBlog, UpdateBlog, DeleteBlog
+        // TODO: AddPost, UpdatePost, DeletePost
+    }
+}
