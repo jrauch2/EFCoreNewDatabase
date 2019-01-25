23275ab65149a9853fc283a292b4992c829ab30c
jgrissom <jgrissom91@gmail.com>
2019-01-24 19:45:48 -0600

    Register Data Context in App Startup
    1) Modify appsettings.json to store database connection information
    2) Register the BloggingContext service during application startup

--- Startup.cs
+++ Startup.cs
@@ -6,15 +6,29 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using EFCoreNewDatabase.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EFCoreNewDatabase
 {
     public class Startup
     {
+        // this class needs the connection info stored in the 
+        // appsettings.json config file - that's a dependency
+        // with dependency injection we expose the config file to this class
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // this is where we use the config info for our connection string
+            services.AddDbContext<BloggingContext>(options => options.UseSqlServer(Configuration["Data:Blog:ConnectionString"]));
             services.AddMvc();
         }
 
